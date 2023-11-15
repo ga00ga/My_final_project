b9a2672103534400c4dc2361d8e75b90d8d2fff8
ga00ga <148340308+ga00ga@users.noreply.github.com>
2023-11-15 19:42:09 +0000

    file handler

---
+++ FileHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace GraphicalCommandInterpreter
+{
+    internal class FileHandler
+    {
+        internal string LoadFromFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SaveToFile(string fileName, string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FileOperationException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace GraphicalCommandInterpreter
+{
+    [Serializable]
+    internal class FileOperationException : Exception
+    {
+        public FileOperationException()
+        {
+        }
+
+        public FileOperationException(string? message) : base(message)
+        {
+        }
+
+        public FileOperationException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected FileOperationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
